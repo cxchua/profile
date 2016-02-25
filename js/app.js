91f7a2a011913dc0f3f93f296129d27ee8ca6f51
Cheng Xun Chua <chengxunchua@Chengs-MacBook-Air.local>
2016-02-26 03:12:41 +0800

    basic profile page

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(function() {
+  // $('#goHomeCx').on('click',function(event){
+  //   $('.face-holder').toggle();
+  // })
+
+  $('#aboutCx').on('click',function(event){
+    $('#aboutMe').toggle();
+  })
+
+  $('#proCx').on('click',function(event){
+    $('#projects').toggle();
+  })
+
+  $('#contactCx').on('click',function(event){
+     $('#contact').toggle();
+  })
+
+});
